388fdf882f8695a243308f53780b7a8e9563e854
Nikita Mashchenko <mashchenko1@icloud.com>
2023-06-02 04:19:19 -0500

    Update SideNav.tsx

--- src/components/NavBar/SideNav/SideNav.tsx
+++ src/components/NavBar/SideNav/SideNav.tsx
@@ -14,6 +14,7 @@ import MobileLogo from "@/assets/Platform/MobileLogo";
 import { sections } from "@/constants/sections";
 import { links } from "@/constants/links";
 import LinkButton from "@/components/Shared/LinkButton/LinkButton";
+import { Link } from "react-scroll";
 
 const SideNav = ({ open, setOpen }: { open: boolean; setOpen: Function }) => {
   return (
@@ -26,9 +27,17 @@ const SideNav = ({ open, setOpen }: { open: boolean; setOpen: Function }) => {
         <Close />
       </CloseBtn>
       {sections.map((section, index) => (
-        <LinkText key={index} open={open}>
-          {section.name}
-        </LinkText>
+        <Link
+          to={section.name}
+          smooth={true}
+          duration={500}
+          key={index}
+          onClick={() => setOpen(false)}
+        >
+          <LinkText key={index} open={open}>
+            {section.name}
+          </LinkText>
+        </Link>
       ))}
       <BottomWrapper open={open}>
         <LinkButtonsWrapper>
